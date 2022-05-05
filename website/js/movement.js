77df2531433a0037e618b4b86d10db83910484f5
Bryant Huynh <bryant.huynh03@gmail.com>
2022-05-05 17:05:59 -0500

    Updated Robotic Arm Controller Page (Reformed finger sliders and added elbow slider)

--- website/js/movement.js
+++ website/js/movement.js
@@ -6,6 +6,10 @@ let slider3 = document.getElementById("range3");
 let output3 = document.getElementById("value3");
 let slider4 = document.getElementById("range4");
 let output4 = document.getElementById("value4");
+let slider5 = document.getElementById("range5");
+let output5 = document.getElementById("value5");
+let slider6 = document.getElementById("range6");
+let output6 = document.getElementById("value6");
 
 // Update the current slider value (each time you drag the slider handle)
 output1.innerHTML = slider1.value;
@@ -24,3 +28,12 @@ output4.innerHTML = slider4.value;
 slider4.oninput = function () {
     output4.innerHTML = this.value;
 };
+output5.innerHTML = slider5.value;
+slider5.oninput = function () {
+    output5.innerHTML = this.value;
+};
+output6.innerHTML = slider6.value;
+slider6.oninput = function () {
+    output6.innerHTML = this.value;
+};
+
